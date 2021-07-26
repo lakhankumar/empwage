d848c88a33573d96745a7d2bfd73fa9a0fc456d4
lakhankumar <lakhankumar997@gmail.com>
2021-07-27 00:52:37 +0530

    Added: monthlysalary

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
 
-empcheck=$((RANDOM%3))
+
 emprateperhr=20
+sum=0
+for ((i=0; i<31; i++))
+do
+empcheck=$((RANDOM%3))
 case $empcheck in 
 	1)
 	emphrs=8
@@ -14,3 +18,7 @@ case $empcheck in
 	;;
 esac
 total=$(($emprateperhr*$emphrs))
+sum=$(($sum+$total))
+done
+echo "monthly salary: " $sum
+
